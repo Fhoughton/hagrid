7f50ba449dbfe7ff0125a689e9178418631f5e0c
FHoughton <fdphoughton@protonmail.com>
2024-09-01 05:11:11 +0100

    Reimplement datdir extraction enough to read file names from dir file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
-use clap::{ValueEnum, Parser, Subcommand};
+use clap::{builder::StringValueParser, Parser, Subcommand, ValueEnum};
+use std::fs::read;
+use std::str;
+
+struct DirFileEntry {
+    name: String,
+    filesize: u32,
+    offset: u32
+}
+
+fn datdir_extract(dat_file: String, dir_file: String, out_path: String) {
+    let dir_contents = std::fs::read(dir_file).unwrap();
+    let file_count = u32::from_le_bytes(dir_contents[0..4].try_into().unwrap());
+
+    let dat_contents = std::fs::read(dat_file).unwrap();
+
+    // Read each file entry from the dat file
+    // To do so we read the dir for the file data
+    // (name, pointer, size) and then use these to
+    // Extract the file from the .dat
+    for i in 0..file_count {
+        let dir_offset : usize = 4 + (i * 20) as usize; // entry Each is 20 bytes long
+        let filename_bytes = &dir_contents[dir_offset..dir_offset+12];
+        println!("{:?}", filename_bytes);
+
+        let filename_string = match str::from_utf8(filename_bytes) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        println!("{}", filename_string);
+    }
+}
 
 #[derive(Parser)]
 #[command(name = "hagrid", about = "A file extractor for the PS1 Harry Potter games")]
@@ -33,8 +65,9 @@ fn main() {
     match args.cmd {
         Commands::DatDir{mode, dat_file, dir_file, path} => {
             match mode {
+                // e.g. hagrid dat-dir --mode extract --dat-file potter01_extracted/POTTER.DAT --dir-file potter01_extracted/POTTER.DIR --path out
                 FileMode::Extract => {
-
+                    datdir_extract(dat_file, dir_file, path);
                 }
 
                 FileMode::Pack => {
