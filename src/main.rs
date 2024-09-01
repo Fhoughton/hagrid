b6c3860cb8c84509b61f351b7ae5e6d34fac96f2
FHoughton <fdphoughton@protonmail.com>
2024-09-01 03:32:12 +0100

    Prepare for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::process;
+
+#[derive(Debug)]
+enum FileMode {
+    Extract,
+    Pack,
+}
+
+fn get_arguments() {
+    let args: Vec<_> = std::env::args().collect(); // get all arguments passed to app
+    println!("{:?}", args);
+}
+
 fn main() {
+    get_arguments();
     println!("Hello, world!");
 }
